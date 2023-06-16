4feaf765cdd9149c1acad4bd931feabbd0172495
Xavier Nuñes (Líder de innovación ) <linnovacion@INVERVAL.LOCAL>
2023-06-16 16:00:58 -0400

    app vibrador

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# haptics
+Haptics
+cd Haptics
+npm install
+ionic serve
+npx cap add android
+npm run build
+npx cap sync
+npx cap open android
+luego que abra android estudio esperar que instale las dependencias, buscar la opcion Build, luego Build blunde apk y build apk
+Dependencias que se instalo
+npm install @capacitor/core
+npm install @capacitor/android
+npm install @capacitor/haptics
---
+++ src/views/HomePage.vue
@@ -0,0 +1,88 @@
+<template>
+  <ion-page>
+    <ion-content :fullscreen="true">
+      <div id="container">
+        <ion-button @click="triggerHapticFeedback">Trigger Haptic Feedback</ion-button>
+        <ion-button @click="hapticsImpactMedium">hapticsImpactMedium</ion-button>
+        <ion-button @click="hapticsImpactLight">hapticsImpactLight</ion-button>
+        <ion-button @click="hapticsVibrate">hapticsVibrate</ion-button>
+        <ion-button @click="hapticsSelectionStart">hapticsSelectionStart</ion-button>
+        <ion-button @click="hapticsSelectionChanged">hapticsSelectionChanged</ion-button>
+        <ion-button @click="hapticsSelectionEnd">hapticsSelectionEnd</ion-button>
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import { Haptics, ImpactStyle } from '@capacitor/haptics';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HapticsExample',
+  setup() {
+    const triggerHapticFeedback = async () => {
+      try {
+        await Haptics.impact({ style: 'heavy' });
+      } catch (err) {
+        console.error('Error triggering haptic feedback:', err);
+      }
+    };
+
+    const hapticsImpactMedium = async () => {
+      await Haptics.impact({ style: ImpactStyle.Medium });
+    };
+
+    const hapticsImpactLight = async () => {
+      await Haptics.impact({ style: ImpactStyle.Light });
+    };
+
+    const hapticsVibrate = async () => {
+      await Haptics.vibrate();
+    };
+
+    const hapticsSelectionStart = async () => {
+      await Haptics.selectionStart();
+    };
+
+    const hapticsSelectionChanged = async () => {
+      await Haptics.selectionChanged();
+    };
+
+    const hapticsSelectionEnd = async () => {
+      await Haptics.selectionEnd();
+    };
+    return { triggerHapticFeedback, hapticsImpactMedium, hapticsImpactLight, hapticsVibrate, hapticsSelectionStart, hapticsSelectionChanged, hapticsSelectionEnd};
+  },
+});
+</script>
+
+<style scoped>
+#container {
+  text-align: center;
+  
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+#container strong {
+  font-size: 20px;
+  line-height: 26px;
+}
+
+#container p {
+  font-size: 16px;
+  line-height: 22px;
+  
+  color: #8c8c8c;
+  
+  margin: 0;
+}
+
+#container a {
+  text-decoration: none;
+}
+</style>
